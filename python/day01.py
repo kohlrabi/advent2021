3f355342775bcf5d976f0187d1a15323a876bb67
Jan Christoph Terasa <christoph@kohlio.de>
2021-12-10 22:23:24 +0100

    Solve day 01, pt. 2.

--- python/day01.py
+++ python/day01.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 import fileinput
+import itertools
 
 
 def part1(numbers):
     s = sum(n > m for m, n in zip(numbers[:-1], numbers[1:]))
     return s
 
+def part2(numbers):
+    s = 0
+    prev_window = 3 * max(numbers)
+    for i in range(len(numbers) - 2):
+        window = sum(numbers[i:i+3])
+        if window > prev_window:
+            s += 1
+        prev_window = window
+    return s
+
 
 def main():
     d = [int(line) for line in fileinput.input()]
     print(part1(d))
+    print(part2(d))
 
 
 if __name__ == '__main__':
