af96ee2f407e609b575b39c719026f90a6873d21
Jan Christoph Terasa <christoph@kohlio.de>
2021-12-11 16:08:10 +0100

    Use pairwise and triplewise for day 01.

--- python/day01.py
+++ python/day01.py
@@ -3,21 +3,28 @@
 import fileinput
 import itertools
 
+def pairwise(iterable):
+    x, y = itertools.tee(iterable)
+    next(y, None)
+    return zip(x, y)
+
+def triplewise(iterable):
+    for (a, _), (b, c) in pairwise(pairwise(iterable)):
+        yield a, b, c
 
 def part1(numbers):
-    s = sum(n > m for m, n in zip(numbers[:-1], numbers[1:]))
+    s = sum(n > m for m, n in pairwise(numbers))
     return s
 
 def part2(numbers):
     s = 0
-    prev_window = 3 * max(numbers)
-    for i in range(len(numbers) - 2):
-        window = sum(numbers[i:i+3])
-        if window > prev_window:
+    last = None
+    for a in triplewise(numbers):
+        x = sum(a)
+        if last is not None and x > last:
             s += 1
-        prev_window = window
+        last = x
     return s
-
 
 def main():
     d = [int(line) for line in fileinput.input()]
