9446217eec2ce6c595b4e1065df15cadf35ed89d
Jan Christoph Terasa <christoph@kohlio.de>
2021-12-12 00:13:01 +0100

    Cleanup day 08 a bit.

--- python/day08.py
+++ python/day08.py
@@ -48,6 +48,7 @@ seg2digs = {
     'g': (0, 2, 3, 5, 6, 8, 9)
 }
 
+
 def part1(all_numbers):
     s = 0
     for allnums in all_numbers:
@@ -63,6 +64,8 @@ def part2(all_digits, all_numbers):
     for digits, numbers in zip(all_digits, all_numbers):
         translation = {}
 
+        print(digits)
+
         nums = {
             1: digits[0],
             7: digits[1],
@@ -70,61 +73,53 @@ def part2(all_digits, all_numbers):
             8: digits[-1]
         }
 
-        translation['a'] = tuple(nums[7].difference(nums[1]))[0]
-
         twothreefive = digits[3:6]
         for num in twothreefive:
-            if num == num.union(nums[1]):
-                three = num
+            print(set(num), set(nums[1]))
+            if set(num) == set(num).union(set(nums[1])):
+                nums[3] = num
                 break
-        twothreefive.remove(three)
-        nums[3] = three
-        translation['b'] = tuple(nums[4].difference(three))[0]
+        twothreefive.remove(nums[3])
+        translation['b'] = tuple(set(nums[4]).difference(set(nums[3])))[0]
 
-        x = nums[4].difference(nums[1])
+        x = set(nums[4]).difference(set(nums[1]))
         x.remove(translation['b'])
         translation['d'] = tuple(x)[0]
 
         for num in twothreefive:
             if 'b' in num:
-                five = num
+                nums[5] = num
                 break
-        twothreefive.remove(five)
-        nums[5] = five
+        twothreefive.remove(nums[5])
 
-        two = twothreefive[0]
-        nums[2] = two
+        nums[2] = twothreefive[0]
 
         zerosixnine = digits[6:9]
         for num in zerosixnine:
             if translation['d'] not in num:
-                zero = num
+                nums[0] = num
                 break
-        nums[0] = zero
-        zerosixnine.remove(zero)
+        zerosixnine.remove(nums[0])
 
         for num in zerosixnine:
-            if num.union(nums[1]) == num:
-                nine = num
+            if set(num).union(set(nums[1])) == set(num):
+                nums[9] = num
                 break
-        nums[9] = nine
-        zerosixnine.remove(nine)
+        zerosixnine.remove(nums[9])
 
         nums[6] = zerosixnine[0]
 
         print(nums)
 
-        mapping = {tuple(sorted(tuple(v))): k for k, v in nums.items()}
+        mapping = {tuple(v): k for k, v in nums.items()}
 
         n = ''
         for num in numbers:
-            t = tuple(sorted(tuple(num)))
-            n += str(mapping[t])
+            n += str(mapping[tuple(num)])
 
         s += int(n)
 
     return s
-
 
 
 def main():
@@ -132,11 +127,11 @@ def main():
     all_numbers = []
     for line in fileinput.input():
         digits, numbers = line.strip().split('|')
-        digits = [set(d) for d in digits.strip().split()]
-        numbers = [set(n) for n in numbers.strip().split()]
+        digits = [sorted(d) for d in digits.strip().split()]
+        numbers = [sorted(n) for n in numbers.strip().split()]
         all_digits.append(sorted(digits, key=lambda x: len(x)))
         all_numbers.append(numbers)
-        
+
     print(part1(all_numbers))
     print(part2(all_digits, all_numbers))
 
